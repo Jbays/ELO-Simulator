d2a8397ada6df9d3327167075533a880095163ff
jbays <jjbaize@gmail.com>
2016-06-18 08:54:55 -0500

    The-tournament, outcomeCalculator refactored

--- the-tournament.js
+++ the-tournament.js
@@ -16,17 +16,49 @@ var competitorB = {
   losses: 0
 }
 
-var ratingsAdjuster = function(competitorA,competitorB){
+var ratingsAdjuster = function(competitorA,competitorB) {
 
   var competitorARating = competitorA.rating
   var competitorBRating = competitorB.rating
+  var recordWinsForA   = competitorA.wins
+  var recordWinsForB   = competitorB.wins
+  var recordLossesForA = competitorA.losses
+  var recordLossesForB = competitorB.losses
 
-  var probabilityOfVictoryForA = 1 / (1 + Math.pow(10,((competitorBRating-competitorARating)/400)))
-  var probabilityOfVictoryForB = 1 / (1 + Math.pow(10,((competitorARating-competitorBRating)/400)))
+  var probabilityOfVictoryForA = 1 / (1 + Math.pow(10, ((competitorBRating - competitorARating) / 400)))
+  var probabilityOfVictoryForB = 1 / (1 + Math.pow(10, ((competitorARating - competitorBRating) / 400)))
 
-  console.log("probabilityOfVictoryForA:",probabilityOfVictoryForA)
-  console.log("probabilityOfVictoryForB:",probabilityOfVictoryForB)
+  // console.log("probabilityOfVictoryForA:", probabilityOfVictoryForA)
+  // console.log("probabilityOfVictoryForB:", probabilityOfVictoryForB)
 
+  var outcomeCalculator = function (probabilityOfVictoryForA) {
+
+    var randomNumber     = Math.random()
+
+    if ( probabilityOfVictoryForA > randomNumber ) {
+
+      recordWinsForA++
+      recordLossesForB++
+
+      competitorA.wins   = recordWinsForA
+      competitorB.losses = recordLossesForB
+
+    } else {
+
+      recordLossesForA++
+      recordWinsForB++
+
+      competitorA.losses = recordLossesForA
+      competitorB.wins   = recordWinsForB
+
+    }
+
+    console.log("competitorA:",competitorA)
+    console.log("competitorB:",competitorB)
+
+  }
+
+  outcomeCalculator(probabilityOfVictoryForA,probabilityOfVictoryForB)
 
 }
 
