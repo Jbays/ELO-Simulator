d4e14264d6818631973cb0a50652cf783fe43817
jbays <jjbaize@gmail.com>
2017-05-13 10:51:23 -0500

    Loading and deloading working for compMats

--- index.js
+++ index.js
@@ -2,6 +2,15 @@
 
 const _ = require('underscore');
 
+//NOTE: 5-13-2017
+//the 2015 Mundials had 1861 competitors
+//whose proportions between belts were:
+//if 25 competitors --> = [2,4,7,9,3];
+//WHERE index=0 is black belts
+//index=1 is brown belts
+//...
+//index=4 is white belts
+
 /**
  * @name: generateCompetitors
  * @description: creates "n" number of com
@@ -12,27 +21,67 @@ function generateCompetitors(numOfCompetitors){
 	let outputArr = [];
 
 	for (let i=0; i<numOfCompetitors ;i++ ){
-    let competitorBlueprint = '{"c' + i.toString() + '":{"rating":1600,"wins":0,"losses":0,"compHistory":"","record":""}}';
+    let competitorBlueprint = '{"c' + (i+1).toString() + '":{"rating":1600,"wins":0,"losses":0,"compHistory":"","record":""}}';
 
     outputArr.push(JSON.parse(competitorBlueprint))
 	}
+
+	//10:35am - what to do about competitors's belts??
+
 	return outputArr
 }
 
 /**
- * @name: makeCompetitorsCompete
+ * @name: makeAllCompete
  * @description: 
  * @param: 
  * @returns:
  **/
-function makeCompetitorsCompete(numberOfRounds,everySingleCompetitorArr){
+function makeAllCompete(numberOfRounds,everySingleCompetitorArr){
 	console.log("numberOfRounds>>>>",numberOfRounds);
 	console.log("everySingleCompetitorArr>>>>",everySingleCompetitorArr);
 
+	//for every pair of competitors
+	//push them into the competition mats
+	//make them compete
+	//return them to the finishedCompeting line
+
+	// this will run once for each number of rounds
+	// for (let i = 1;i<numberOfRounds+1;i++){
+	// 	console.log("this is the round number",i)
+	// }
+
+	let finishedArr = [];
+	let compMats = [];
+
+	for (let j = 0; j < everySingleCompetitorArr.length;j++){
+		compMats.push(everySingleCompetitorArr.pop(),everySingleCompetitorArr.pop())
+
+		console.log("compMats",compMats)
+
+		finishedArr.push(compMats.pop(),compMats.pop())
+	}
+
+	console.log("these competitors finished competing",finishedArr)
+
+
+
+
+
+
 	//what needs to happen to make two competitors compete?
 
-	//we need to isolate two competitors from the bullPen (aka everySingleCompetitorArr)
+	//make competitors compete 
 	
+
+	//we need to isolate two competitors from the bullPen (aka everySingleCompetitorArr)
+
+	//we need a ref
+	//we need a scorekeeper
+	//we need competition mats
+
+
+
 
 
 }
@@ -49,16 +98,13 @@ function runTournament(roundNum,tournSize){
 	console.log("running a tournament of !!!",roundNum,"rounds !!!")
 	console.log("and                     !!!",tournSize,"competitors !!!")
 
-	const allCompetitors = generateCompetitors(tournSize)
-
-	makeCompetitorsCompete(roundNum,generateCompetitors(tournSize))
-
-
-	// console.log("these are our competitors>>>>",allCompetitors)
-
-	//creator competitors
+	//this line will create all competitorObjs
+	//and put them in an array
+	//generateCompetitors(tournSize)
 
 	//make them compete
+	makeAllCompete(roundNum,generateCompetitors(tournSize))
+
 
 	//afterward I'll have some interesting choices:
 
@@ -67,11 +113,9 @@ function runTournament(roundNum,tournSize){
 
 	//2. or make basic ui?
 
-	
 
 
 	// return ...what?
-
 }
 
 //the simplest tournament is two rounds, four competitors
