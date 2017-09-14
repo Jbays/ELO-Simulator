52634c317bacb713901260f9c88b4248f99681e8
Justin Baize <justin.baize@gJBc897ad.local>
2017-09-14 18:43:00 -0500

    First commit.  Still very confused.

---
+++ parseCsv.js
@@ -0,0 +1,13 @@
+const csvParse = require('csv-parse');
+// const ebiRawData = require('./ebi-2-results-competitor-list.csv');
+// const ebiRecords = require('./ebi2.js');
+// console.log(ebiRecords);
+// const ebiCsv = require('../ebi-2-results-competitor-list.csv')
+// const ebiCompList = csvParse('./ebi-2-results-competitor-list.csv');
+
+//this doesn't work
+// const ebiRecords2 = require('../ebi2.js');
+// console.log(ebiRecords2);
+
+//FINAL THOUGHTS
+//HOW THE HELL DO I IMPORT CSV DATA?
