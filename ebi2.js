acf63ed001c748853e46370e2c27a080c272484b
Justin Baize <justin.baize@gJBc897ad.local>
2017-10-13 08:30:21 -0500

    Adding files

---
+++ ebi2.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const records = `,,,
+  ,EDDIE BRAVO INVITATIONAL 2,,
+  ,"October 10, 2014",,
+  ,,,
+  ,,,
+  ,135,NAME,ACADEMY
+  ,1,Geo Martinez,10th Planet San Diego
+  ,2,Javier Cardenas,Paragon Santa Maria
+  ,3,Julian Medrano,Camarillo Jiu Jitsu
+  ,4,Kerry Phan,10th Planet Costa Mesa/Buena Park
+  ,5,Kristian Woodmansee,Atos
+  ,6,Richard Alarcon,Lotus Club
+  ,7,Randall Dolf,10th Planet Riverside
+  ,8,Sean Najjar,Baret Submissions
+  ,9,Alexander Condos,Marcelo Garcia Academy
+  ,10,Alexis Alduncin,Renzo Gracie Mexico
+  ,11,Alex Canders,10th Planet San Francisco
+  ,12,Geoff Real,Victory MMA
+  ,13,Rene Lopez,Zenith BJJ
+  ,14,Eric Medina,Jean Jacques Machado Academy
+  ,15,Matt Betzold,Ultimate Fitness/Team Alpha Male
+  ,16,Fabio Passos,Cobrinha Brazilian Jiu Jitsu
+  ,,,
+  ,155,NAME,ACADEMY
+  ,1,Denny Prokopos,10th Planet San Francisco
+  ,2,Rich Chavez,Judgement MMA and Fitness
+  ,3,Eddie Fyvie,Spa City Jiu-Jitsu
+  ,4,Ben Jimenez,Cobrinha Brazilian Jiu Jitsu
+  ,5,Russ Miura,Subfighter
+  ,6,Matthew Kaiser,Fierce Studio Martial Arts
+  ,7,Jeremy Fields,10th Planet Corona
+  ,8,Pete Fabela,Paragon Santa Maria
+  ,9,Nathan Orchard,10th Planet Portland
+  ,10,PJ Lindner,Xtreme Couture
+  ,11,Dane Molina,5 Star Martial Arts/Renzo Gracie LA
+  ,12,Joe Murphy,Cleber Jiu Jitsu
+  ,13,Rafael Domingos,Zenith BJJ
+  ,14,Juan Bernardo,Victory MMA
+  ,15,Adam Morrison,Fight Strong
+  ,16,Baret Yoshida,Baret Submissions
+`;
+
+module.export = records;
---
+++ realTournament/realComp.js
@@ -0,0 +1,78 @@
+const allCompetitors = require('./parseCsv.js');
+const runCompetitionMats = require('../index.js');
+const _ = require('underscore');
+
+let shuffledCompetitors = _.shuffle(allCompetitors);
+
+//these two lines will create n number of competitors
+//then shuffle the competitors into the real competitor data
+let artificialCompetitors = makeDummyCompetitors(32);
+shuffledCompetitors = _.shuffle(shuffledCompetitors.concat(artificialCompetitors));
+
+/**
+ * @name: makeDummyCompetitors
+ * @description: creates "n" number of competitors
+ * @param: numOfCompetitors (integer) - the amount of competitors to be generated
+ * @returns: all competitors in a shuffled array
+ **/
+function makeDummyCompetitors(numOfCompetitors){
+	let outputArr = [];
+
+	//for-loop which will generate competitors equaling to numOfCompetitors
+	for (let i=0;i<numOfCompetitors;i++){
+    let competitorBlueprint = '{"name":"'+
+															'c'+(i+1).toString()+
+															'","teamName":"generic","bracket":"n/a","rating":1600,"wins":0,"losses":0,"streak":"","compRecord":""}';
+
+    outputArr.push(JSON.parse(competitorBlueprint))
+	}
+
+	return _.shuffle(outputArr)
+}
+
+const seasonedCompetitors = runCompetitionMats(5,shuffledCompetitors,64,200);
+
+// console.log("seasonedCompetitors>>>",seasonedCompetitors);
+//NOTE: I don't think points are being added to the rating 100% correctly.
+//NOTE: The first batch appears to work.  seasonedCompetitors.
+//NOTE: But I'm unsure about the second.
+// console.log("second batch of competitions!",runCompetitionMats(1,seasonedCompetitors,32,200));
+
+/**
+ * @name: runTypicalTournament
+ * @description:
+ * @param: allCompetitors
+ * @returns: allCompetitors after completing a proper, single-elimination tournament
+ **/
+function runTypicalTournament(allCompetitors){
+	console.log("this is allCompetitors>>>",allCompetitors);
+}
+
+runTypicalTournament(seasonedCompetitors);
+
+//where runCompetitionMats(numberOfRounds,competitors,kFactor,classSize)
+//Param1: numberOfRounds is the number of rounds each competitor will compete!
+//Param2: shuffledCompetitors are the competitor objects to compete!
+//Param3: kFactor is the amount of points to be waged PER MATCH.
+//Param4: classSize determines the amount of points difference
+//NOTE:   Probability of victory/loss for rating difference of one classSize
+//NOTE:   Rating separation of ONE-HALF class
+//NOTE:   64%/36% WHERE 64% prob of victory belongs to the higher-rated player.
+//NOTE:   64%/36% WHERE 36% prob of victory belongs to the lower-rated player.
+//NOTE:   Rating separation of ONE class
+//NOTE:   76%/24% WHERE 76% prob of victory belongs to the higher-rated player.
+//NOTE:   76%/24% WHERE 24% prob of victory belongs to the lower-rated player.
+//NOTE:   Rating separation of THREE-HALF classes
+//NOTE:   85%/15% WHERE 85% prob of victory belongs to the higher-rated player.
+//NOTE:   85%/15% WHERE 15% prob of victory belongs to the lower-rated player.
+//NOTE:   Rating separation of TWO classes
+//NOTE:   91%/9% WHERE 91% prob of victory belongs to the higher-rated player.
+//NOTE:   91%/9% WHERE 9% prob of victory belongs to the lower-rated player.
+// console.log(runCompetitionMats(30,shuffledCompetitors,64,200));
+
+// ** Note, that FIDE changed the K-factors as of July 2014 (see FIDE rating regulations - chapter 8.56):
+// K = 40 for a player new to the rating list until he has completed events with at least 30 games
+// K = 20 as long as a player's rating remains under 2400.
+// K = 10 once player's rating reached 2400.
+// K = 40 for all players until their 18th birthday, as long as their rating remains under 2300.
+//NOTE: 2400 converts to black belt.  GUESS?!
